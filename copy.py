389dd6f1d4dca86869cff65d8418481cdebce173
kylevandermeulen97 <59993688+kylevandermeulen97@users.noreply.github.com>
2020-04-23 11:55:47 -0700

    Delete copy.py

--- copy.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect("host=localhost dbname=space_time_app user=postgres")
-cur = conn.cursor()
-with open('/Users/KyleVanderMeulen/Desktop/EGR400-SpaceTimeAnalysis/usa_covid_cases.csv', 'r') as f:
-    # Notice that we don't need the `csv` module.
-    next(f)  # Skip the header row.
-    cur.copy_from(f, 'covid')
-
-conn.commit()
